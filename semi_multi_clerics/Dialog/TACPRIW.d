b9ff4df69a2f108b4c182159cef4b937f4c99547
11jo <79876551+11jo@users.noreply.github.com>
2024-11-18 17:43:43 +0100

    component

--- semi_multi_clerics/Dialog/TACPRIW.d
+++ semi_multi_clerics/Dialog/TACPRIW.d
@@ -1,14 +1,12 @@
 BEGIN ~tacprsp~
 
 IF ~~ THEN BEGIN holaIWD
-SAY @10
-IF ~~ THEN REPLY @11 GOTO ciaoIWD
-IF ~XPGT(LastTalkedToBy,4999)~ THEN REPLY @12 DO ~StartStore("tacprsp",LastTalkedToBy)~ EXIT
+	SAY @10
+	IF ~~ THEN REPLY @11 GOTO ciaoIWD
+	IF ~XPGT(LastTalkedToBy,4999)~ THEN REPLY @12 DO ~StartStore("tacprsp",LastTalkedToBy)~ EXIT
 END
 
 IF ~~ THEN BEGIN ciaoIWD
-SAY @14
-IF ~~ THEN EXIT
-END
-
+	SAY @14
+	IF ~~ THEN EXIT
 END
--- semi_multi_clerics/Dialog/tacprsp.d
+++ semi_multi_clerics/Dialog/tacprsp.d
@@ -1,18 +1,16 @@
 BEGIN ~tacprsp~
 
 // BP2 TO ADD
-IF ~OR(3)
-AreaCheck("AR0700")
-AreaCheck("ar4500")
-~ THEN BEGIN hola
-SAY @10
-IF ~~ THEN REPLY @11 GOTO ciao
-IF ~XPGT(LastTalkedToBy,46999)~ THEN REPLY @12 DO ~StartStore("tacprsp",LastTalkedToBy)~ EXIT
+IF ~
+	OR(3)
+		AreaCheck("AR0700")
+		AreaCheck("ar4500")~ THEN BEGIN hola
+	SAY @10
+	IF ~~ THEN REPLY @11 GOTO ciao
+	IF ~XPGT(LastTalkedToBy,46999)~ THEN REPLY @12 DO ~StartStore("tacprsp",LastTalkedToBy)~ EXIT
 END
 
 IF ~~ THEN BEGIN ciao
-SAY @14
-IF ~~ THEN EXIT
+	SAY @14
+	IF ~~ THEN EXIT
 END
-
-
