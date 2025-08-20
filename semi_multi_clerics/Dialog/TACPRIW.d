cbcebcd2c92261be0dbd20afc33e711cb321d3a9
11jo <79876551+11jo@users.noreply.github.com>
2025-08-20 03:37:34 +0200

    oIWD untested

--- semi_multi_clerics/Dialog/TACPRIW.d
+++ semi_multi_clerics/Dialog/TACPRIW.d
@@ -3,7 +3,7 @@ BEGIN ~tacprsp~
 IF ~~ THEN BEGIN holaIWD
 	SAY @10
 	IF ~~ THEN REPLY @11 GOTO ciaoIWD
-	IF ~XPGT(LastTalkedToBy,4999)~ THEN REPLY @12 DO ~StartStore("TACPRSP",LastTalkedToBy)~ EXIT
+	IF ~XPGT(LastTalkedToBy,4999)~ THEN REPLY @12 DO ~StartStore("TACPRSP",LastTalkedToBy) SetGlobal("TACPRSP_TALKED","LOCALS",2)~ EXIT
 END
 
 IF ~~ THEN BEGIN ciaoIWD
